f07ab1a5167ea416e478f7cd7a64c43be25333f5
LeMari <lm_@mail.ru>
2016-12-22 01:50:25 +0400

    Главная страница - адаптивные изображения + svg + активные/ховер-состояния элементов

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 var nav = document.querySelector('.nav');
 var header_title = document.querySelector('.header__title');
 var button_toggle = document.querySelector('.nav-toggle');
+var burger_toggle = document.querySelector('.header__burger');
+var cross_toggle = document.querySelector('.header__cross');
 button_toggle.addEventListener('click', function (event) {
   event.preventDefault();
   nav.classList.toggle('nav_active');
   header_title.classList.toggle('header__title_active');
+  burger_toggle.classList.toggle('header__burger_hidden');
+  cross_toggle.classList.toggle('header__cross_hidden');
 });
