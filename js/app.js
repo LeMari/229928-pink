27141a951b049a85be10c8388ae4170c59c77dcf
LeMari <lm_@mail.ru>
2016-12-25 21:18:26 +0400

    Правки после первой финальной проверки

--- js/app.js
+++ js/app.js
@@ -3,6 +3,12 @@ var header_title = document.querySelector('.header__title');
 var button_toggle = document.querySelector('.nav-toggle');
 var burger_toggle = document.querySelector('.header__burger');
 var cross_toggle = document.querySelector('.header__cross');
+
+nav.classList.remove('nav_active');
+burger_toggle.classList.remove('header__burger_hidden');
+header_title.classList.remove('header__title_active');
+cross_toggle.classList.add('header__cross_hidden');
+
 button_toggle.addEventListener('click', function (event) {
   event.preventDefault();
   nav.classList.toggle('nav_active');
